ff6994f2fe28ab3c4b65a5a153f0c8938b38ae8c
Deisy <deisyu98@gmail.com>
2025-04-04 15:59:45 -0500

    Par e impar

---
+++ Conceptos-Programacion/PareImpar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("DIGITE UN NUMERO: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("EL NUMERO ES PAR");
+        }
+        else
+        {
+            Console.WriteLine("EL NUMERO ES IMPAR");
+        }
